4a0ec6bc4c93f018a65590bcfa7c51852f84a699
Jordan McKible <jordan@mckible.com>
2010-10-05 13:12:38 -0700

    Not ancient unit test formatting.

--- test/has_password_test.rb
+++ test/has_password_test.rb
@@ -1,16 +1,23 @@
 require 'test_helper'
 
 class HasPasswordTest < ActiveSupport::TestCase
+  def setup
+    @person = people(:hal)
+  end
 
   #####################################################################
   #                     A U T H E N T I C A T I O N                   #
   #####################################################################
-  def test_sha1
+  test 'sha1' do
     assert_equal Digest::SHA1.hexdigest('passwordsalt'), Person.encrypt('password', 'salt')
   end
   
-  def test_valid_authentication
-    assert_equal people(:hal), Person.authenticate(people(:hal).email, 'password')
+  test 'valid authentication' do 
+    assert_equal @person, Person.authenticate(@person.email, 'password')
+  end
+  
+  test 'invalid password' do
+    assert_nil Person.authenticate(@person.email, 'invalid')
   end
   
 end
