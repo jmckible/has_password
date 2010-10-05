264fa41544df2842473d0ef6cca1947234be1486
Jordan McKible <jordan@mckible.com>
2010-10-05 13:32:01 -0700

    Ported tests.

--- test/db/schema.rb
+++ test/db/schema.rb
@@ -1,5 +1,6 @@
 ActiveRecord::Schema.define(:version=>0) do
   create_table :people, :force=>true do |t|
     t.string :email, :password_hash, :password_salt, :password_reset_token
+    t.boolean :force_change_password, :default=>false
   end
 end
--- test/has_password_test.rb
+++ test/has_password_test.rb
@@ -6,7 +6,7 @@ class HasPasswordTest < ActiveSupport::TestCase
   end
 
   #####################################################################
-  #                     A U T H E N T I C A T I O N                   #
+  #                     C L A S S    M E T H O D S                    #
   #####################################################################
   test 'sha1' do
     assert_equal Digest::SHA1.hexdigest('passwordsalt'), Person.encrypt('password', 'salt')
@@ -16,6 +16,9 @@ class HasPasswordTest < ActiveSupport::TestCase
     assert_equal 8, Person.generate_password.length
   end
   
+  #####################################################################
+  #                     A U T H E N T I C A T I O N                   #
+  #####################################################################
   test 'valid authentication' do 
     assert_equal @person, Person.authenticate(@person.email, 'password')
   end
@@ -30,6 +33,90 @@ class HasPasswordTest < ActiveSupport::TestCase
   
   test 'no password' do
     assert_nil Person.authenticate(@person.email, nil)
+  end
+  
+  #####################################################################
+  #               P A S S W O R D    A T T R I B U T E                #
+  #####################################################################
+  test 'virtual password attribute' do
+    assert @person.respond_to?(:password)
+    assert @person.respond_to?(:password=)
+  end
+  
+  test 'change password with confirmation' do
+    @person.password = 'newpassword11'
+    @person.password_confirmation = 'newpassword11'
+    @person.save
+    assert_equal @person, Person.authenticate(@person.email, 'newpassword11')
+  end
+  
+  test 'password reset token generation' do
+    @person.set_reset_token
+    assert_equal 20, @person.password_reset_token.length
+  end
+  
+  test 'password reset' do
+    assert_equal @person, Person.authenticate(@person.email, 'password')
+    @person.reset_password
+    assert @person.save
+    assert_nil Person.authenticate(@person.email, 'password')
+    assert @person.force_change_password?
+    assert_nil @person.password_reset_token
+  end
+  
+  #####################################################################
+  #                          L I F E C Y C L E                        #
+  #####################################################################
+  test 'encrypt password before save' do
+    person = Person.new :email=>'new@email.com', :password=>'password1', :password_confirmation=>'password1'
+    assert person.save
+    assert !person.password_hash.empty?
+    assert !person.password_salt.empty?
+  end
+  
+  #####################################################################
+  #                       V A L I D A T I O N S                       #
+  #####################################################################
+  test 'password confirmation' do
+    person = Person.new :password=>'password'
+    assert !person.valid?
+    assert !person.errors.on(:password_confirmation).blank?
+  end
+  
+  test 'minimum password length of 6' do
+    person = Person.new :password=>'12345'
+    assert !person.valid?
+    assert !person.errors.on(:password).blank?
+  end
+  
+  test 'maximum password length of 40' do
+    person = Person.new :password=>'1                                        '
+    assert !person.valid?
+    assert !person.errors.on(:password).blank?
+  end
+  
+  test 'passwords need one integer' do
+    person = Person.new :password=>'abcdefg'
+    assert !person.valid?
+    assert !person.errors.on(:password).blank?
+  end
+  
+  #####################################################################
+  #                        P R O T E C T I O N S                      #
+  #####################################################################
+  test 'do not update password hash through mass assignment' do
+    @person.update_attributes! :password_hash=>'updated'
+    assert_not_equal 'updated', @person.password_hash
+  end
+  
+  test 'do not update password salt through mass assignment' do
+    @person.update_attributes! :password_salt=>'updated'
+    assert_not_equal 'updated', @person.password_salt
+  end
+  
+  test 'do not update password reset token through mass assignment' do
+    @person.update_attributes! :password_reset_token=>'updated'
+    assert_not_equal 'updated', @person.password_reset_token
   end
   
 end
